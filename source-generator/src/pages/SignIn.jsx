95bf31897e92593741c1ce468938d243fb022e69
Nath9666 <nathan.morel@efrei.net>
2024-02-20 23:41:09 +0100

    animation infinite start fournisseur for singIn

--- source-generator/src/pages/SignIn.jsx
+++ source-generator/src/pages/SignIn.jsx
@@ -2,12 +2,112 @@ import React, { useEffect, useState } from "react"
 import { Data } from "../components"
 
 export default function Home() {
+  const [formUser, setformUser] = useState({
+    username: "",
+    password: "",
+  })
+
+  const fournisseur = [
+    {
+      name: "Google",
+      logo: Data.GoogleLogo,
+      action: () => {
+        console.log("Google")
+      },
+      color: "bg-red-500",
+      active: true,
+    },
+    {
+      name: "Facebook",
+      logo: Data.FacebookLogo,
+      action: () => {
+        console.log("Facebook")
+      },
+      color: "bg-blue-500",
+      active: false,
+    },
+    {
+      name: "Twitter",
+      logo: Data.TwitterLogo,
+      action: () => {
+        console.log("Twitter")
+      },
+      color: "bg-blue-400",
+      active: false,
+    },
+    {
+      name: "Github",
+      logo: Data.GithubLogo,
+      action: () => {
+        console.log("Github")
+      },
+      color: "bg-black",
+      active: false,
+    },
+    {
+      name: "Microsoft",
+      logo: Data.MicrosoftLogo,
+      action: () => {
+        console.log("Microsoft")
+      },
+      color: "bg-green-600",
+      active: false,
+    },
+  ]
+
   return (
-    <div>
-      <div className="body">
-        <h1>Bienvenue sur {Data.name}</h1>
-        <p>{Data.ShortDescriptionFrench}</p>
-        <p>{Data.LongDescriptionFrench}</p>
+    <div className="flex items-center min-h-screen">
+      <div className="animate-shadow-pulse rounded-xl m-auto">
+        <div className="m-10 text-2xl text-main_color">
+          <h1>Bienvenue sur {Data.name}</h1>
+        </div>
+      </div>
+      <div className="animate-shadow-pulse rounded-xl m-auto">
+        <div className="flex flex-col justify-center m-10 items-center gap-y-4 ">
+          <div className="flex flex-col">
+            <label htmlFor="username">Nom d'utilisateur</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={formUser.username}
+              onChange={(e) => setformUser(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="password">Mot de passe</label>
+            <input
+              className="rounded-md border-2 border-main_color"
+              type="password"
+              id="password"
+              name="password"
+              value={formUser.password}
+              onChange={(e) => setformUser(e.target.value)}
+            />
+          </div>
+          <div className="flex justify-around m-4 gap-3">
+            {fournisseur.map((fournisseur) => (
+              <button
+                className={`text-white rounded-full w-10 h-10 ${fournisseur.color}`}
+                key={fournisseur.name}
+                onClick={fournisseur.action}
+                disabled={fournisseur.active}
+              >
+                <img
+                  className="h-full"
+                  src={fournisseur.logo}
+                  alt={fournisseur.name}
+                />
+              </button>
+            ))}
+          </div>
+          <button
+            className="bg-main_color text-white p-2 rounded-md"
+            onClick={console.log(formUser)}
+          >
+            Se connecter
+          </button>
+        </div>
       </div>
     </div>
   )
--- source-generator/tailwind.config.js
+++ source-generator/tailwind.config.js
@@ -10,17 +10,27 @@ module.exports = {
         secondary_color_light: "#d9e5eb",
       },
       screens: {
-        'tablet': '640px',
+        tablet: "640px",
         // => @media (min-width: 640px) { ... }
-  
-        'laptop': '1024px',
+
+        laptop: "1024px",
         // => @media (min-width: 1024px) { ... }
-  
-        'desktop': '1920px',
+
+        desktop: "1920px",
         // => @media (min-width: 1280px) { ... }
 
-        'tele': '2000px',
+        tele: "2000px",
         // => @media (min-width: 1500px) { ... }
+      },
+      keyframes: {
+        "shadow-pulse": {
+          "0%": { boxShadow: "0 0 5px #60a3bc" },
+          "50%": { boxShadow: "0 0 20px #000, 0 0 15px #000" },
+          "100%": { boxShadow: "0 0 5px #60a3bc" },
+        },
+      },
+      animation: {
+        "shadow-pulse": "shadow-pulse 4s cubic-bezier(0, 0, 0.2, 1) infinite",
       },
     },
   },
